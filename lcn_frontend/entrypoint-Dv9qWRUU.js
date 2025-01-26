7090b64d05c1d3abf601c1fe1fa0787b2e29164b
Andre Lengwenus <alengwenus@gmail.com>
2025-01-26 16:23:01 +0000

    Update

--- lcn_frontend/entrypoint-Dv9qWRUU.js
+++ lcn_frontend/entrypoint-Dv9qWRUU.js
@@ -1,14 +1,14 @@
 
 function loadES5() {
   var el = document.createElement('script');
-  el.src = '/lcn_static/frontend_es5/entrypoint-9xD187NX.js';
+  el.src = '/lcn_static/frontend_es5/entrypoint-yGurv7E_.js';
   document.body.appendChild(el);
 }
 if (/.*Version\/(?:11|12)(?:\.\d+)*.*Safari\//.test(navigator.userAgent)) {
     loadES5();
 } else {
   try {
-    new Function("import('/lcn_static/frontend_latest/entrypoint-6lqG534V.js')")();
+    new Function("import('/lcn_static/frontend_latest/entrypoint-Dv9qWRUU.js')")();
   } catch (err) {
     loadES5();
   }
